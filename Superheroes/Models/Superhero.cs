00eb394070a3598a48d0ca40be99349bb137f941
gabrielkunkel <henrysaltandson@gmail.com>
2019-10-24 12:33:39 -0500

    add [key] to superhero model

--- Superheroes/Models/Superhero.cs
+++ Superheroes/Models/Superhero.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace Superheroes.Models
 {
     public class Superhero
     {
+        [Key]
         public int superheroId { get; set; }
         public string name { get; set; }
         public string alterEgo { get; set; }
