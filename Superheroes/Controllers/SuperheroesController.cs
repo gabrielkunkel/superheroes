0ee08d0d67896fd3062ee79223b7eeba99276492
gabrielkunkel <henrysaltandson@gmail.com>
2019-10-24 16:57:44 -0500

    add Create method on SuperheroesController

--- Superheroes/Controllers/SuperheroesController.cs
+++ Superheroes/Controllers/SuperheroesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Superheroes.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,13 @@ namespace Superheroes.Controllers
 {
     public class SuperheroesController : Controller
     {
+        ApplicationDbContext dbContext;
+
+        public SuperheroesController()
+        {
+            dbContext = new ApplicationDbContext();
+        }
+
         // GET: Superheroes
         public ActionResult Index()
         {
@@ -23,17 +31,19 @@ namespace Superheroes.Controllers
         // GET: Superheroes/Create
         public ActionResult Create()
         {
-            return View();
+            Superhero superhero = new Superhero();
+            return View(superhero);
         }
 
         // POST: Superheroes/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Superhero superhero)
         {
             try
             {
                 // TODO: Add insert logic here
-
+                dbContext.Superheroes.Add(superhero);
+                dbContext.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
@@ -50,7 +60,7 @@ namespace Superheroes.Controllers
 
         // POST: Superheroes/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int id, Superhero superhero)
         {
             try
             {
@@ -72,7 +82,7 @@ namespace Superheroes.Controllers
 
         // POST: Superheroes/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(int id, Superhero superhero)
         {
             try
             {
