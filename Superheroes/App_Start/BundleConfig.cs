205b258a20147a0f32f3dc6e5d24783e198b5ade
gabrielkunkel <henrysaltandson@gmail.com>
2019-10-24 19:47:55 -0500

    change bootstrap theme

--- Superheroes/App_Start/BundleConfig.cs
+++ Superheroes/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Superheroes
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-3.css",
                       "~/Content/site.css"));
         }
     }
